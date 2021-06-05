a1ce146f340104464a03345fa5024fea7361fed5
kudzai.macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-06-05 11:24:38 +0200

    Initial commit

---
+++ src/main/java/zw/co/footprint/kaguvi/KaguviApplication.java
@@ -0,0 +1,13 @@
+package zw.co.footprint.kaguvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaguviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaguviApplication.class, args);
+	}
+
+}
